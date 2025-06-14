747be8c78af7afa50163214ab65703d165d111fd
ilanmk98 <ilanmk98@gmail.com>
2025-06-14 12:16:42 -0300

    creacion BE

---
+++ challenge-BE/src/main/java/com/ilanmk/challenge_BE/ChallengeBeApplication.java
@@ -0,0 +1,13 @@
+package com.ilanmk.challenge_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeBeApplication.class, args);
+	}
+
+}
---
+++ challenge-BE/src/test/java/com/ilanmk/challenge_BE/ChallengeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilanmk.challenge_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
